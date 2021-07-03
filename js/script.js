147140598870f9f882b0d0c5001d21a7f451b2aa
Isradin <isradin.nurbek@gmail.com>
2021-07-03 04:26:37 +0300

    Create js file & did task in lesson 02

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 'use strict';
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+const money = 70000,
+    income = 'Фриланс',
+    addExpenses = 'Интернет, Такси, Квартира',
+    deposit = false,
+    mission = 200000,
+    period = 7,
+    budgetDay = money / 30;
 
-alert('Hello World!');
-console.log('Hello JS!');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split());
+console.log(budgetDay.toFixed());
