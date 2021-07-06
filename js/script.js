321a079bb0a86f843c3fa63edbbefc16271f10fe
Isradin <isradin.nurbek@gmail.com>
2021-07-06 22:40:24 +0300

    Create js file & complete task in lesson05-complicated

--- js/script.js
+++ js/script.js
@@ -1,55 +1,23 @@
 'use strict';
 
-const money = +prompt('Ваш месячный доход?'),
-    income = 'Фриланс',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    mission = 200000,
-    period = 7,
-    expenses1 = prompt('Введите обязательную статью расходов?'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    expenses2 = prompt('Введите обязательную статью расходов?'),
-    amount2 = +prompt('Во сколько это обойдется?');
+const arr = [];
 
-const getExpensesMonth = function() {
-    return amount1 + amount2;
-};
+arr.push('258', '354', '681', '445', '217', '905', '759');
 
-const getAccumulatedMonth = function() {
-    return money - getExpensesMonth();
-};
+console.log(arr.filter(function(num) {
+    return num.includes(['2', '4'][0]);
+}));
 
-const accumulatedMonth = getAccumulatedMonth();
+let num = 100;
 
-const getTargetMonth = function() {
-    return Math.ceil(mission / accumulatedMonth);
-};
-
-const budgetDay = Math.floor(accumulatedMonth / 30);
-
-const showTypeOf = function(data) {
-    return typeof data;
-};
-
-
-console.log(showTypeOf(money));
-console.log(showTypeOf(income));
-console.log(showTypeOf(deposit));
-console.log('Расходы за месяц: ' + getExpensesMonth());
-console.log(addExpenses.toLowerCase().split(', '));
-console.log('Цель будет достигнута за: ' + getTargetMonth() + ' месяцев');
-console.log('Бюджет на день: ' + budgetDay);
-
-const getStatusIncome = function() {
-    if (budgetDay >= 1200) {
-        return 'У вас высокий уровень дохода';
-    } else if (600 <= budgetDay && budgetDay < 1200) {
-        return 'У вас средний уровень дохода';
-    } else if (0 < budgetDay && budgetDay < 600) {
-        return 'К сожалению у вас уровень дохода ниже среднего';
-    } else if (budgetDay <= 0) {
-        return 'Что то пошло не так';
+for (let i = 2; i <= num; i++) {
+    let point = 1;
+    for (let j = 2; j <= i / 2 && point === 1; j++) {
+        if (i % j === 0) {
+            point = 0;
+        }
     }
-};
-
-console.log(getStatusIncome());
+    if (point === 1) {
+        console.log(i + ' - Делители этого числа: 1 и ' + i);
+    }
+}
