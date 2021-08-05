e9d7796aafa8bfee11fca15e084eed2c20969e71
Isradin <isradin.nurbek@gmail.com>
2021-08-06 00:24:47 +0300

    Complete task

--- js/script.js
+++ js/script.js
@@ -1,57 +1,63 @@
-window.addEventListener('DOMContentLoaded', () => {
-    // Timer
-    const timer = (idSelector, deadline) => {
+// eslint-disable-next-line strict
+'use strict';
 
-        const getTimeRemaining = endTime => {
-            const timeDifference = Date.parse(endTime) - Date.parse(new Date()),
-                seconds = Math.floor((timeDifference / 1000) % 60),
-                minutes = Math.floor((timeDifference / 1000 / 60) % 60),
-                hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+const toLog = () => {
+    const date = new Date(),
+        hours = date.getHours(),
+        day = date.getDay(),
+        days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-            return {
-                'total': timeDifference,
-                seconds,
-                minutes,
-                hours
-            };
-        };
+    const greeting = () => {
+        let goodDay,
+            dayNow;
 
-        const addZero = num => {
-            if (num >= 0 && num < 10) {
-                return `0${num}`;
-            } else {
-                return num;
+        if (hours >= 3 && hours < 9) {
+            goodDay = 'Доброе утро';
+        } else if (hours >= 9 && hours < 15) {
+            goodDay = 'Добрый день';
+        } else if (hours >= 15 && hours < 21) {
+            goodDay = 'Добрый вечер';
+        } else {
+            goodDay = 'Доброй ночи';
+        }
+
+        days.forEach((item, index) => {
+            if (index === day) {
+                dayNow = item;
             }
+        });
+
+        return {
+            goodDay,
+            dayNow
         };
-
-        const setClock = (selector, endTime) => {
-            const timer = document.querySelector(selector),
-                hours = timer.querySelector('#timer-hours'),
-                minutes = timer.querySelector('#timer-minutes'),
-                seconds = timer.querySelector('#timer-seconds'),
-                timeInterval = setInterval(updateClock, 1000);
-
-            updateClock(); // Stops blinking layout (Мигание верстки) when we update the page
-
-            function updateClock() {
-                const remainingTime = getTimeRemaining(endTime);
-
-                hours.textContent = addZero(remainingTime.hours);
-                minutes.textContent = addZero(remainingTime.minutes);
-                seconds.textContent = addZero(remainingTime.seconds);
-
-                if (remainingTime.total <= 0) {
-                    hours.textContent = '00';
-                    minutes.textContent = '00';
-                    seconds.textContent = '00';
-
-                    clearInterval(timeInterval);
-                }
-            }
-        };
-
-        setClock(idSelector, deadline);
     };
 
-    timer('#timer', '2021-08-29');
-});
+    const toNewYear = () => {
+        const newYearDate = new Date('January 1 2022').getTime(),
+            nowDate = new Date().getTime(),
+            timeRemaining = (newYearDate - nowDate) / 1000,
+            timeRemainingDays = Math.floor(timeRemaining / 60 / 60 / 24);
+
+        return timeRemainingDays;
+    };
+
+    const declOfNum = (n, titles) => n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+        0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
+    const toHtml = () => {
+        const timeString = new Date().toLocaleTimeString('en');
+
+        document.body.innerHTML =
+            `<i>${greeting().goodDay}</i><br>
+	            <i>Сегодня: ${greeting().dayNow}</i><br>
+	            <i>Текущее время: ${timeString}</i><br>
+	            <i>До Нового Года осталось: ${declOfNum(toNewYear(), ['день', 'дня', 'дней'])} </i>
+	        `;
+    };
+
+    toHtml();
+    setInterval(toHtml, 1000);
+};
+
+toLog();
